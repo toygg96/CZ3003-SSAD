62f6f1f6a1e66dc51d5b24e3cff80d37ab8a94a2
toygg96 <josephkingyiu19@gmail.com>
2019-10-03 20:09:19 +0800

    Profile function created

--- CZ3003-QuizGame/end/interface/MenuGame.gd
+++ CZ3003-QuizGame/end/interface/MenuGame.gd
@@ -5,3 +5,6 @@ func _on_Quit_pressed():
 
 func _on_Credits_pressed():
 	return get_tree().change_scene("res://interface/screen_credits.tscn")
+	
+func _on_Profile_pressed():
+	return get_tree().change_scene("res://interface/profile/UserProfile.tscn")
