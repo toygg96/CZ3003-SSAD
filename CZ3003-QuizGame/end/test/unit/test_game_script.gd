898b5c1f834bb452a014d53409602ce0e5008e07
TryingOutSomething <43298887+TryingOutSomething@users.noreply.github.com>
2019-10-29 17:12:19 +0800

    edit testing leaderboard

---
+++ CZ3003-QuizGame/end/test/unit/test_game_script.gd
@@ -0,0 +1,10 @@
+extends "res://addons/gut/test.gd"
+
+var Game = load('res://interface/leaderboard/leaderboard.gd')
+
+func test_get_set_question():
+    gut.p('Testing create user')
+    var game = Game.new()
+    var test_question = ["tom", "harry", "tim"]
+    assert_accessors(game, "user", [], test_question)
+    
