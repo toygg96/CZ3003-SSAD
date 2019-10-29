73d6c26fc901d1982ea76f1d93d3f8fdd6c6daec
TryingOutSomething <43298887+TryingOutSomething@users.noreply.github.com>
2019-10-29 17:17:19 +0800

    edit function name for test leaderboard

--- CZ3003-QuizGame/end/test/unit/test_leaderboard.gd
+++ CZ3003-QuizGame/end/test/unit/test_leaderboard.gd
@@ -2,7 +2,7 @@ extends "res://addons/gut/test.gd"
 
 var Game = load('res://interface/leaderboard/leaderboard.gd')
 
-func test_get_set_question():
+func test_get_set_leaderboard():
     gut.p('Testing create user')
     var game = Game.new()
     var test_question = ["tom", "harry", "tim"]
