5b5691155ddc9ed61672b6f2f0e48f7a28edd611
TryingOutSomething <43298887+TryingOutSomething@users.noreply.github.com>
2019-10-31 18:21:41 +0800

    Added summary report feature

--- CZ3003-QuizGame/end/interface/summaryreport/Summary.gd
+++ CZ3003-QuizGame/end/interface/summaryreport/Summary.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var http : HTTPRequest = $HTTPRequest
 onready var notification = get_node("Score container/scores/report")
+onready var search_input = get_node("Score container/searchbar/SearchInput")
 
 var user_list = []
 var average_overall_score = 0
@@ -11,6 +12,7 @@ var average_w3_score = 0
 var average_w4_score = 0
 var average_w5_score = 0
 var average_w6_score = 0
+var defaultText = ""
 var profile = {} 
 
 func _ready():
@@ -53,13 +55,27 @@ func generate_scores():
 	average_w5_score /= user_list.size()
 	average_w6_score /= user_list.size()
 	
-	print("ave overall: ", average_overall_score)
-	print("w1: ", average_w1_score)
-	print("w2: ", average_w2_score)
-	print("w3: ", average_w3_score)
-	print("w4: ", average_w4_score)
-	print("w5: ", average_w5_score)
-	print("w6: ", average_w6_score)
+	defaultText = "Average Overall Score: " + str(average_overall_score) + "\nAverage World 1 Score: " + str(average_w1_score) + "\nAverage World 2 Score: " + str(average_w2_score) + "\nAverage World 3 Score: " + str(average_w3_score) + "\nAverage World 4 Score: " + str(average_w4_score) + "\nAverage World 5 Score: " + str(average_w5_score) + "\nAverage World 6 Score: " + str(average_w6_score)
+	notification.text = defaultText
 	
-	notification.text = "Average Overall Score: " + str(average_overall_score) + "\nAverage World 1 Score: " + str(average_w1_score) + "\nAverage World 2 Score: " + str(average_w2_score) + "\nAverage World 3 Score: " + str(average_w3_score) + "\nAverage World 4 Score: " + str(average_w4_score) + "\nAverage World 5 Score: " + str(average_w5_score) + "\nAverage World 6 Score: " + str(average_w6_score)
+func _on_search_button_pressed():
+	var username = ""
+	for user in user_list:
+		username = user.name.split("/")[6]
+		if search_input.text != username:
+			continue
+			
+		# If user is in the list, display info and exit from function
+		notification.text = "User: " + username + "\nNickname: " + user.fields.nickname.stringValue + "\nWorld 1 Score: " + user.fields.W1Score.integerValue + "\nWorld 2 Score:" + user.fields.W2Score.integerValue + "\nWorld 3 Score:" + user.fields.W3Score.integerValue + "\nWorld 4 Score:" + user.fields.W4Score.integerValue + "\nWorld 5 Score:" + user.fields.W5Score.integerValue + "\nWorld 6 Score:" + user.fields.W6Score.integerValue
+		return
 	
+	# If user not found
+	notification.text = "User does not exist"
+		
+func _on_SearchInput_text_changed(new_text):
+	if new_text.length() == 0:
+		notification.text = defaultText
+		
+func _on_back_pressed():
+	return get_tree().change_scene("res://interface/MenuGame.tscn")
+
